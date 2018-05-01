7fe32a55b18bc7e5f873c936a9a798053c50cb79
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-05-01 14:42:24 -0700

    allow for multiple download files

--- main/helpers.py
+++ main/helpers.py
@@ -10,10 +10,11 @@ def get_runkeeper_file(oh_member):
                             client_id=settings.OPENHUMANS_CLIENT_ID,
                             client_secret=settings.OPENHUMANS_CLIENT_SECRET)
         user_object = api.exchange_oauth2_member(oh_access_token)
+        files = {}
         for dfile in user_object['data']:
             if 'Runkeeper' in dfile['metadata']['tags']:
-                return dfile['download_url']
-        return ''
+                files[dfile['basename']] = dfile['download_url']
+        return files
     except:
         return 'error'
 
